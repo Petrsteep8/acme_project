669d4febbe078cd514d7c070073e0a4492b5436b
Peter(HSE) <prsalmin@edu.hse.ru>
2024-11-17 12:41:16 +0300

    Добавил регистрацию, логализацию, ограничение прав и тд

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,18 +5,33 @@ from django.views.generic import (
     ListView, CreateView, UpdateView, DeleteView, DetailView
 )
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
 
-class BirthdayCreateView(CreateView):
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
+
+
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     form_class = BirthdayForm
     model = Birthday
 
-class BirthdayUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class BirthdayUpdateView(OnlyAuthorMixin, UpdateView):
     form_class = BirthdayForm
     model = Birthday
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(OnlyAuthorMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
@@ -27,7 +42,7 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayDetailView(DetailView):
+class BirthdayDetailView(OnlyAuthorMixin, DetailView):
     model = Birthday
 
     def get_context_data(self, **kwargs):
@@ -36,3 +51,8 @@ class BirthdayDetailView(DetailView):
             self.object.birthday
         )
         return context
+    
+
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
